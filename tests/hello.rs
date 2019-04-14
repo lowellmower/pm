6d2ab0ac48ed5126af2ca3bc5f9e23fff7346ae1
Lowell <lowell.mower@gmail.com>
2019-04-14 10:33:25 -0400

    two structs can do that same thing

--- tests/hello.rs
+++ tests/hello.rs
@@ -4,8 +4,16 @@ use pm::HelloMacro;
 #[derive(Debug, HelloMacro)]
 struct Test {}
 
+#[derive(Debug, HelloMacro)]
+struct TestTwo {}
+
 #[test]
-fn works() {
+fn an_arbitrary_id_is_returned() {
     let id = Test::get_id();
     assert!(id == 9);
+}
+
+#[test]
+fn two_structs_both_display_behavior() {
+    assert!(Test::get_id() == TestTwo::get_id());
 }
