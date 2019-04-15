6f42b43d4ca2138c89f6f82581181c2271922065
Lowell <lowell.mower@gmail.com>
2019-04-15 05:32:18 -0400

    add test case and struct reading

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,35 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::*;
 use quote::quote;
+use syn::*;
 
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_get_id(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
-    // create an arbitrary public function on the AST 
+    // create an arbitrary public function on the AST
     // derived name and tokenize for return
     let result = quote! {
         impl #name {
             pub fn get_id() -> u8 { 9 }
+        }
+    };
+
+    result.into()
+}
+
+#[proc_macro_derive(HelloFields)]
+pub fn hello_fields_print_fields(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident_name = &input.ident;
+    // let data = &input.data;
+
+    let result = quote! {
+        impl #ident_name {
+            fn print_name(&self) -> String {
+                self.name.clone()
+            }
         }
     };
 
--- tests/hello.rs
+++ tests/hello.rs
@@ -1,11 +1,20 @@
 extern crate pm;
-use pm::HelloMacro;
+use pm::*;
 
 #[derive(Debug, HelloMacro)]
 struct Test {}
 
-#[derive(Debug, HelloMacro)]
-struct TestTwo {}
+#[derive(Debug, HelloMacro, HelloFields)]
+struct TestTwo {
+    pub id: u8,
+    pub name: String,
+}
+
+impl TestTwo {
+    pub fn new(i: u8, n: String) -> TestTwo {
+        TestTwo { id: i, name: n }
+    }
+}
 
 #[test]
 fn an_arbitrary_id_is_returned() {
@@ -16,4 +25,24 @@ fn an_arbitrary_id_is_returned() {
 #[test]
 fn two_structs_both_display_behavior() {
     assert!(Test::get_id() == TestTwo::get_id());
+}
+
+#[test]
+fn a_struct_can_still_be_implemented_in_main() {
+    let id = 10;
+    let name = "foo".to_string();
+
+    let tt = TestTwo::new(id, name);
+
+    assert!(TestTwo::get_id() != tt.id);
+}
+
+#[test]
+fn a_struct_print_the_field_name() {
+    let id = 10;
+    let name = "foo".to_string();
+
+    let tt = TestTwo::new(id, name.clone());
+
+    assert!(tt.print_name() == name);
 }
